57d0a7c59afe74d7182920140967518d9191e577
denis <tashirevd@mail.ru>
2023-05-16 12:27:08 +0300

    first commit

---
+++ game_of_life.py
@@ -0,0 +1,7 @@
+import random
+import numpy as np
+import seaborn as sns
+import time
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
